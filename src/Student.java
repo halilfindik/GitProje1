68784ec4b591f9f6c3dd7665048107856526c5e5
halilfindik <halfin@gmail.com>
2022-07-04 21:58:33 +0300

    Student class initiated

--- src/Student.java
+++ src/Student.java
@@ -1,5 +1,9 @@
 public class Student {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        System.out.println("Hello again");
+
+        System.out.println(4+5);
     }
 }
